7f82da489bdab33bb1b1b8bf80f367ae457e519d
nick <nick@solar>
2019-07-15 22:20:27 -0400

    home page 90% done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -61,6 +61,7 @@ export default class Home extends Component {
 
             // set JSON data
             data = JSON.parse(decoded_data);
+            console.log(data);
         })
         
         .then(() => {
@@ -109,18 +110,41 @@ export default class Home extends Component {
             // get the parseable data
             this.read_user_data(response);
         });
-
     }
+
 
     show_data = () => {
         if (this.state.user_data_loaded && this.state.top_players_loaded) {
             return(
-                this.state.top_players.map((player,index) => 
-                    <div className="player-wrapper" key={index}>
-                        <span className="user-name">{this.state.top_players[index].user_name}</span>
-                        <span className="score">{this.state.top_players[index].score} Points</span>
+                <div className="data-wrapper">
+                    <div className="player-data">
+                        <div className="indiv-player-wrapper">
+                            <span className="user-name">{this.state.user_data.user_name} <span className="color">(you)</span></span>
+                            <span className="score">{this.state.user_data.score} Points</span>
+                            <div className="rank-wrapper">
+                                <span className="rank-label">Rank</span>
+                                <span className="rank-value">{this.state.user_data.rank}</span>
+                            </div>
+                        </div>
                     </div>
-                )
+                    <div className="switch-wrapper">
+                        <div className="top-players-button">
+                            <span>Top 10 Players</span>
+                        </div>
+                    </div>
+                    <div className="top-players-wrapper">
+                    {this.state.top_players.map((player,index) => 
+                        <div className="player-wrapper" key={index}>
+                            {index === 0 ?
+                            <span className="user-name top">{this.state.top_players[index].user_name}</span>
+                            : 
+                            <span className="user-name">{this.state.top_players[index].user_name}</span>
+                            }
+                            <span className="score">{this.state.top_players[index].score} Points</span>
+                        </div>
+                    )}
+                    </div>
+                </div>
             );    
         } else {
             return(
